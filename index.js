648eeb5f572f4ab8649b919a9715cdcbd0e73223
Nikka Hassan <nikka.hassan@gmail.com>
2019-01-14 16:23:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function theBeatlesPlay(musicians, instruments) {
   var x = []
   for ( var i = 0;i < musicians.length; i++) {
     x.push(musicians[i] + 'plays' instruments[i]) 
-    
   }
+  return x 
 }
